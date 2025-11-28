d01442a25e2e7bcd5a23e47efd28f9acc2db0015
Chandrashekara K R <chandrashekara.kr@amd.com>
2025-11-28 11:57:29 +0530

    Refactor AOCL-BIY build configuration:
    - Added CMake logic to comment out AOCL_UTILS_LIB setting in ${LIBM_DIR}/src/CMakeLists.txt.
    - Added ENABLE_AOCL_LIBMEM option in linux-make.json presets for all build configurations.

--- aocl_libm_build.cmake
+++ aocl_libm_build.cmake
@@ -71,6 +71,15 @@ else()
     set(UTILS_LIB "${CMAKE_BINARY_DIR}/aocl-utils/install_package/lib/${Linux_Utils_Lib_Name}")
 endif()
 
+# Replace string in ${LIBM_DIR}/src/CMakeLists.txt
+set(target_file "${LIBM_DIR}/src/CMakeLists.txt")
+file(READ "${target_file}" file_content)
+# Regex pattern to match the line (allow spaces)
+set(pattern "set\\(AOCL_UTILS_LIB[ \t]+\\$\\{AOCL_UTILS_LIB_DIR\\}/\\$\\{AOCL_UTILS_LIB\\}\\)")
+# Replace with empty string
+string(REGEX REPLACE "${pattern}" "" file_content "${file_content}")
+file(WRITE "${target_file}" "${file_content}")
+
 # Log the configuration command
 file(APPEND "${LIBM_BUILD_LOG_FILE_PATH}" "CONFIGURATION COMMAND: cmake -G \"${CMAKE_GENERATOR}\" -S ${LIBM_DIR} -B ${CMAKE_BINARY_DIR}/aocl-libm/build_dir -DCMAKE_CONFIGURATION_TYPES=${CMAKE_CONFIGURATION_TYPES} -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DAOCL_UTILS_INCLUDE_DIR=${CMAKE_BINARY_DIR}/aocl-utils/install_package/include -DAOCL_UTILS_LIB=${UTILS_LIB} -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/aocl-libm/install_package ${CompilerToolSet}.\n")
 
